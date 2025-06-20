5614a15e114b1873be0c5cd6e91f3b68ccec3dc8
parkjioh <jioh035@kakao.com>
2025-06-20 14:19:45 +0900

    lab 1

---
+++ src/main/java/com/back/Main.java
@@ -0,0 +1,18 @@
+package com.back;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        lab1();
+    }
+
+    private static void lab1() {
+        String input = "등록";
+        Scanner scanner = new Scanner(input);
+        String cmd = scanner.nextLine();
+
+        System.out.println(cmd);
+    }
+
+}
