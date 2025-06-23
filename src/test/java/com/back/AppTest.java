a67626c5c7d28006b2f473ebd7777ecdd0191997
parkjioh <jioh035@kakao.com>
2025-06-23 15:44:17 +0900

    step 7,g

--- src/test/java/com/back/AppTest.java
+++ src/test/java/com/back/AppTest.java
@@ -79,7 +79,7 @@ public class AppTest {
         assertThat(rs)
                 .contains("번호 / 작가 / 명언")
                 .contains("---------------------")
-                .contains("2 / 작자미상 / 과거에 집착하지 마라.")
-                .contains("1 / 작자미상 / 현재를 사랑하라.");
+                .contains("2 / 작가미상 / 과거에 집착하지 마라.")
+                .contains("1 / 작가미상 / 현재를 사랑하라.");
     }
 }
