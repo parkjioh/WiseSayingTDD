0e6cffbb471f2907c562bbf9a66a5ea716e7bdde
parkjioh <jioh035@kakao.com>
2025-06-23 17:03:35 +0900

    step 7, b

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -1,11 +1,10 @@
 package com.back;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private final Scanner scanner;
+
     public App(Scanner scanner) {
         this.scanner = scanner;
     }
@@ -13,34 +12,19 @@ public class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
-        int lastId = 0;
+        WiseSayingController wiseSayingController = new WiseSayingController(scanner);
+        SystemController systemController = new SystemController();
 
-        List<WiseSaying> wiseSayings = new ArrayList<>();
        while (true){
            System.out.print("명령) ");
            String cmd = scanner.nextLine();
+
+
            switch (cmd) {
-               case "등록" -> {
-                   System.out.print("명언 : ");
-                   String content = scanner.nextLine();
-
-                   System.out.print("작가 : ");
-                   String author = scanner.nextLine();
-
-                   int id = ++lastId;
-
-                   wiseSayings.add(new WiseSaying(id, content, author));
-
-                   System.out.printf("%d번 명언이 등록되었습니다.\n", id);
-               }
-               case "목록" ->{
-                   System.out.println("번호 / 작가 / 명언");
-                   System.out.println("---------------------");
-                   for ( WiseSaying wiseSaying : wiseSayings.reversed()){
-                       System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent());
-                   }
-               }
+               case "등록" ->  wiseSayingController.actionWrite();
+               case "목록" ->  wiseSayingController.actionList();
                case "종료" -> {
+                   systemController.actionExit();
                    return;
                }
            }
---
+++ src/main/java/com/back/SystemController.java
@@ -0,0 +1,8 @@
+package com.back;
+
+public class SystemController {
+    public void actionExit() {
+        System.out.println("프로그램이 종료되었습니다.");
+
+    }
+}
