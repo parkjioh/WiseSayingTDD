6d49f12c8c429662e4d5e3b10aa0204920c7f02c
parkjioh <jioh035@kakao.com>
2025-07-01 10:48:04 +0900

    step 32,g

--- src/main/java/com/back/domain/wiseSaying/repository/WiseSayingFileRepository.java
+++ src/main/java/com/back/domain/wiseSaying/repository/WiseSayingFileRepository.java
@@ -106,7 +106,7 @@ public class WiseSayingFileRepository implements WiseSayingRepository {
         );
     }
 
-    private List<WiseSaying> findAll() {
+    public List<WiseSaying> findAll() {
         return loadAllWiseSayings()
                 .sorted(Comparator.comparingInt(WiseSaying::getId).reversed())
                 .toList();
@@ -142,6 +142,7 @@ public class WiseSayingFileRepository implements WiseSayingRepository {
                 .map(Util.file.json::toMap)
                 .map(WiseSaying::new);
     }
+
 
 
 }
--- src/main/java/com/back/domain/wiseSaying/repository/WiseSayingRepository.java
+++ src/main/java/com/back/domain/wiseSaying/repository/WiseSayingRepository.java
@@ -1,9 +1,12 @@
 package com.back.domain.wiseSaying.repository;
 
 import com.back.domain.wiseSaying.entity.WiseSaying;
+import com.back.global.Appconfig;
 import com.back.standard.dto.Page;
 import com.back.standard.dto.Pageable;
+import com.back.standard.util.Util;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface WiseSayingRepository {
@@ -21,5 +24,26 @@ public interface WiseSayingRepository {
 
     Page<WiseSaying> findforListByContentContainingOrAuthorContaining(String keyword1, String keyword2, Pageable pageable);
 
-    default String archive(){ return null; };
+    List<WiseSaying> findAll();
+
+    default String archive(){
+        List<WiseSaying> all = findAll();
+
+        String json = Util.json.toString(
+                all
+                        .stream()
+                        .map(WiseSaying::toMap)
+                        .toList()
+        );
+
+        String filePath = getArchiveFilePath();
+
+        Util.file.set(filePath, json);
+
+        return filePath;
+    }
+
+    default String getArchiveFilePath(){
+        return Appconfig.getMode() + "Db/wiseSaying/data.json";
+    }
 }
